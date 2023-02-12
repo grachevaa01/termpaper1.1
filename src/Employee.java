9307aae4c0f8245a45da587b7595d11328cbba39
grachevaa01 <grachevaa01@mail.ru>
2023-02-12 13:54:24 +0300

    Курсовая работа. Задача базовый уровень.

---
+++ src/Employee.java
@@ -0,0 +1,44 @@
+public class Employee {
+    static int idGenerator = 1;
+    int id;
+    String fullName;
+    int department;
+    double salary;
+
+    public Employee(String fullName, int department, double salary) {
+        this.fullName = fullName;
+        this.department = department;
+        this.salary = salary;
+        this.id = idGenerator++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString (){
+        return "ID: " + this.id + " ФИО " +  this.fullName + " Отдел: " + this.department + "Зарплата: " + this.salary;
+    }
+
+}
